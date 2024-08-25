7486a54c4f9c165279dd689a69f46292229ab49d
Tiallysson Costa <tiallysson@proton.me>
2024-08-25 13:33:05 -0300

    Adicionados alguns dados pré modulados de Tasks

--- Contexts/NextCodersDbContext.cs
+++ Contexts/NextCodersDbContext.cs
@@ -6,7 +6,7 @@ namespace Contexts
     public class NextCodersDbContext : DbContext
     {
         public NextCodersDbContext(DbContextOptions<NextCodersDbContext> options)
-            : base(options) 
+            : base(options)
         {
         }
 
@@ -15,6 +15,30 @@ namespace Contexts
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<TaskModel>().HasData(
+                new TaskModel
+                {
+                    Id = -1,
+                    Title = "Manutenção API PetHealth",
+                    Description = "Continuar a implementação de algunas rotas de Update e Delete",
+                    Status = 0
+                },
+                new TaskModel
+                {
+                    Id = -2,
+                    Title = "Estudos sobre Dependence Injection no C#",
+                    Description = "Estudar sobre como implementar injeção de dependência no projeto",
+                    Status = 0
+                },
+                new TaskModel
+                {
+                    Id = -3,
+                    Title = "Estudar sobre Autenticação com JWT",
+                    Description = "Estudar sobre os paços de implementação do JSON Web Token na minha api To-Do List",
+                    Status = 0
+                }
+                );
+
             base.OnModelCreating(modelBuilder);
         }
     }
